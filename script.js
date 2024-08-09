1ccbc76bde2d2ce6e64ce4ff1fcc8636fc936fbf
dvforest <dvforest@hotmail.com>
2024-08-08 22:44:44 -0400

    Add working subdivisions to the flex-container.
    -The grid is initialized with default subdivs on page load.
    
    - Clicking the button prompts the user to enter new subdivisions.
    
    - Running the subdivideGrid function clears the grid and subdivides the flex-container using the prompted subdivs.

---
+++ script.js
@@ -0,0 +1,30 @@
+// Default grid
+const container = document.querySelector(".flex-container");
+let subdiv = 8;
+subdivideGrid(subdiv);
+
+// Reset button
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", e => {
+    subdiv = parseInt(prompt("Enter subdivisions:", subdiv));
+    subdivideGrid(subdiv);
+});
+
+function subdivideGrid(s){
+    
+    // Clear grid
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    // Subdivide grid
+    for (let i = 0; i < s**2; i++){
+        const div = document.createElement("div");
+        let size = 512 / s;
+        div.style.width = size.toString() + "px";
+        div.style.height = size.toString() + "px";
+        div.style.boxSizing = "border-box";
+        div.classList.toggle("grid");
+        container.appendChild(div);
+    }
+}

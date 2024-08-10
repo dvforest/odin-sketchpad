4b409e80a2f0bd10de34baa86e80bf5a8a4f66e3
dvforest <dvforest@hotmail.com>
2024-08-10 09:02:20 -0400

    Add mouse over event on each div to change the RGBA background.

--- script.js
+++ script.js
@@ -16,19 +16,29 @@ resetButton.addEventListener("click", e => {
 // FUNCTIONS
 
 function subdivideGrid(s){
+    
     // Clear grid
     while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
     // Subdivide grid
     for (let i = 0; i < s**2; i++){
+        
+        // Create one div and set attributes
         const div = document.createElement("div");
         let size = 512 / s;
         div.style.width = size.toString() + "px";
         div.style.height = size.toString() + "px";
         div.style.boxSizing = "border-box";
+        div.style.backgroundColor = "rgba(0,0,0,0)";
         div.classList.toggle("grid");
         div.setAttribute("id", i);
+        
+        // Add mousehover event
+        div.addEventListener("mouseover", (e) => {
+            div.style.backgroundColor = "rgba(0,0,0,1)";
+        });
+        
         container.appendChild(div);
     }
 }
